54c732ac87cfa4c82ca6cc540c826d403743ff16
humberto vela <hvcasas@gmail.com>
2011-08-03 19:59:39 -0700

    second

--- test/functional/shifts_controller_test.rb
+++ test/functional/shifts_controller_test.rb
@@ -1,23 +1,47 @@
 require 'test_helper'
 
 class ShiftsControllerTest < ActionController::TestCase
-  test "should get index" do
+  def setup
+    assert @shift = Factory.create(:shift)
+  end
+  
+  test "can reach index" do
     get :index
     assert_response :success
+    assert_template :index
+    assert_not_nil assigns(:shifts)
   end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
-
-  test "should get new" do
+  test "should get new shift" do
     get :new
     assert_response :success
+    assert_template :new
+  end
+  
+  test "should create shift" do
+    assert_difference('Shift.count') do
+      post :create, :shift => { :description => 'Nocturno 2'}
+    end
+    assert_redirected_to shift_path(assigns(:shift))
   end
 
-  test "should get edit" do
-    get :edit
-    assert_response :success
+  test "can reach edit" do
+    get :edit, :id => @shift.id
+    assert assigns(:shift)
+    assert_equal @shift, assigns(:shift)
+  end
+
+  test "should update shift" do
+    shift = @shift.description
+    put :update, :id => @shift.id, :shift => {:description => 'Nocturno 1'}
+    assert_not_equal shift, @shift.reload.description
+    assert_redirected_to shift_path(assigns(:shift))
+  end
+
+  test "should delete shift" do
+    assert_difference('Shift.count', -1) do
+      delete :destroy, :id => @shift.id
+    end
+    assert_response :redirect
   end
 end
